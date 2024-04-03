4c7df59b11073c326e7ab3ee15206e161a46c31e
mavi <mariapimenta2607@gmail.com>
2024-04-03 16:35:48 -0300

    create view for wallet and profile pic

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,15 @@
 const express = require('express');
 const route = express.Router();
 
+// Iniciando Multer
+const multer = require("multer");
+
+// Recebendo arquivo do multer que criamos
+const config = require('../src/config/multer');
+
+// Home user irá receber um arquivo com o "name" do HTML chamado de "foto"
+route.post('/homeUser', multer(config).single('photo'), cadastro.alunoInsert);
+
 // Importando os Controllers
 const register = require('./controllers/register');
 const home = require('./controllers/home');
